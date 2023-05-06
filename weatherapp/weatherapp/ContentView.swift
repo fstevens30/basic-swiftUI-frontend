0f338b8cfc0c7a4ea5ccb0783e5efbcd37af3d81
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-05-06 17:21:40 +1200

    Modified colours, forecast Hstack, refactored

--- weatherapp/weatherapp/ContentView.swift
+++ weatherapp/weatherapp/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
        
         ZStack {
-            LinearGradient(colors: [.blue, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(colors: [Color("darkBG"), Color("lightBG")], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
             VStack {
                 Text("Christchurch, NZ")
@@ -31,6 +31,18 @@ struct ContentView: View {
                         .foregroundColor(.white)
                         
                 }
+                Spacer()
+                
+                VStack{
+                    
+                    HStack {
+                        WeatherDayView(dayOfWeek: "SUN", imageName: "cloud.drizzle.fill", dailyHigh: 18)
+                        WeatherDayView(dayOfWeek: "MON", imageName: "cloud.heavyrain.fill", dailyHigh: 14)
+                        WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.rain.fill", dailyHigh: 19)
+                        WeatherDayView(dayOfWeek: "WED", imageName: "wind", dailyHigh: 28)
+                        WeatherDayView(dayOfWeek: "THU", imageName: "sun.max.fill", dailyHigh: 31)
+                    }
+                }
                 
                 Spacer()
             }
@@ -41,5 +53,28 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var dailyHigh: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(dailyHigh)°")
+                .font(.system(size:28, weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
